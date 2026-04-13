048f372f8b1f6f46dacb68ed1265d98a44c04b40
ShraddhaMulekar <mulekarshraddha@gmail.com>
2026-04-14 01:30:25 +0530

    login and register controller updated

--- backend/controllers/authController/loginController.js
+++ backend/controllers/authController/loginController.js
@@ -1,3 +1,53 @@
-export const loginController = (req, res) => {
-    res.send("login")
-}
+import bcrypt from "bcrypt";
+import { UserModel } from "../../models/UserModel.js";
+import jwt from "jsonwebtoken";
+
+export const loginController = async (req, res) => {
+  const { email, password } = req.body || {};
+
+  if (!email || !password) {
+    return res
+      .status(400)
+      .json({ message: "Please fill all the fields", ok: false });
+  }
+
+  try {
+    const userMatch = await UserModel.findOne({ email });
+    if (!userMatch) {
+      return res
+        .status(400)
+        .json({ message: "User not found. Please register.", ok: false });
+    }
+
+    bcrypt.compare(password, userMatch.password, async (err, result) => {
+      if (err) {
+        return res.status(500).json({
+          message: "Error comparing passwords",
+          error: err.message,
+          ok: false,
+        });
+      }
+      if (result) {
+        const payload = { name: userMatch.name, email: userMatch.email };
+        const token = jwt.sign(payload, process.env.JWT_SECRET, {
+          expiresIn: "1h",
+        });
+        return res.status(200).json({
+          message: "Login successful!",
+          token,
+          ok: true,
+        });
+      } else {
+        return res
+          .status(400)
+          .json({ message: "Invalid Password!", ok: false });
+      }
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Internal server error",
+      error: error.message,
+      ok: false,
+    });
+  }
+};
--- backend/controllers/authController/registerController.js
+++ backend/controllers/authController/registerController.js
@@ -5,14 +5,18 @@ export const registerController = async (req, res) => {
   const { name, email, password } = req.body || {};
 
   if (!name || !email || !password) {
-    return res.status(400).json({ message: "Please fill all the fields", ok: false });
+    return res
+      .status(400)
+      .json({ message: "Please fill all the fields", ok: false });
   }
 
   try {
     const userMatch = await UserModel.findOne({ email });
 
     if (userMatch) {
-      return res.status(400).json({ message: "User already exists. Please login.", ok: false });
+      return res
+        .status(400)
+        .json({ message: "User already exists. Please login.", ok: false });
     }
 
     bcrypt.hash(
@@ -20,15 +24,33 @@ export const registerController = async (req, res) => {
       Number(process.env.SALT_ROUNDS),
       async (err, hash) => {
         if (err) {
-          return res.status(500).json({ message: "Error hashing password", error: err.message, ok: false });
+          return res
+            .status(500)
+            .json({
+              message: "Error hashing password",
+              error: err.message,
+              ok: false,
+            });
         }
         const newUser = new UserModel({ name, email, password: hash });
         await newUser.save();
 
-        res.status(201).json({ message: "User registered successfully!", newUser: { name, email }, ok: true });
+        res
+          .status(201)
+          .json({
+            message: "User registered successfully!",
+            newUser: { name, email },
+            ok: true,
+          });
       },
     );
   } catch (error) {
-    res.status(500).json({ message: "Internal server error", error: error.message, ok: false });
+    res
+      .status(500)
+      .json({
+        message: "Internal server error",
+        error: error.message,
+        ok: false,
+      });
   }
 };
