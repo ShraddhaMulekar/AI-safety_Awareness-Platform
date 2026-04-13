31fa0225b712a12e8c99529642088e3b4fe2f3ca
ShraddhaMulekar <mulekarshraddha@gmail.com>
2026-04-14 01:18:15 +0530

    register controller logic updated

--- backend/controllers/authController/registerController.js
+++ backend/controllers/authController/registerController.js
@@ -1,3 +1,34 @@
-export const registerController = (req, res) => {
-    res.send("register")
-}
+import { UserModel } from "../../models/UserModel.js";
+import bcrypt from "bcrypt";
+
+export const registerController = async (req, res) => {
+  const { name, email, password } = req.body || {};
+
+  if (!name || !email || !password) {
+    return res.status(400).json({ message: "Please fill all the fields", ok: false });
+  }
+
+  try {
+    const userMatch = await UserModel.findOne({ email });
+
+    if (userMatch) {
+      return res.status(400).json({ message: "User already exists. Please login.", ok: false });
+    }
+
+    bcrypt.hash(
+      password,
+      Number(process.env.SALT_ROUNDS),
+      async (err, hash) => {
+        if (err) {
+          return res.status(500).json({ message: "Error hashing password", error: err.message, ok: false });
+        }
+        const newUser = new UserModel({ name, email, password: hash });
+        await newUser.save();
+
+        res.status(201).json({ message: "User registered successfully!", newUser: { name, email }, ok: true });
+      },
+    );
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error", error: error.message, ok: false });
+  }
+};
