af4b55401b931e2a8c9b378eb41e272aa8c0c99c
ShraddhaMulekar <mulekarshraddha@gmail.com>
2026-04-14 00:47:24 +0530

    Server running and DB connected

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,17 @@
 import express from "express"
+import cors from "cors"
 import dotenv from "dotenv"
+import { connectDB } from "./config/db.js"
 dotenv.config()
 
 const app = express()
 const port = process.env.PORT || 3000
 
-app.listen(port, ()=>{
-    console.log(`Server is running on port ${port}`)
+app.use(express.json())
+app.use(cors())
+
+
+app.listen(port, async()=>{
+    await connectDB()
+    console.log(`Server is running on port http://localhost:${port}`)
 })
