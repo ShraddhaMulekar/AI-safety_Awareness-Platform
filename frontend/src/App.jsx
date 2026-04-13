07219db2745d696036367c5a529675587fe10d5d
ShraddhaMulekar <mulekarshraddha@gmail.com>
2026-04-14 01:41:09 +0530

    vite install for frontend react

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+// import reactLogo from './assets/react.svg'
+// import viteLogo from './assets/vite.svg'
+// import heroImg from './assets/hero.png'
+// import './App.css'
+
+function App() {
+
+  return (
+    <>
+      react
+    </>
+  )
+}
+
+export default App
