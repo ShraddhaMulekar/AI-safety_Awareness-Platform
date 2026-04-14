3e8d945c3ad8b4a1287332863f53c9cc013e87bc
ShraddhaMulekar <mulekarshraddha@gmail.com>
2026-04-14 22:51:58 +0530

    login Logic created

---
+++ frontend/src/components/login/loginLogic.jsx
@@ -0,0 +1,32 @@
+import { useLocation, useNavigate } from "react-router-dom";
+import { useFetch } from "../../hooks/useFetch";
+
+export const loginLogic = () => {
+  const { request } = useFetch();
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const loginUser = async (values) => {
+    try {
+      const data = await request("/auth/login", "POST", values);
+      console.log(data);
+
+      if (data?.token) {
+        localStorage.setItem("token", data.token);
+        alert("Login Successful!");
+
+        const from = location.state?.from;
+        if (from) {
+          navigate(from, { replace: true });
+        } else {
+          navigate("/", { replace: true });
+        }
+      } else {
+        alert(data?.message || "Login failed!");
+      }
+    } catch (error) {
+      throw error;
+    }
+  };
+  return { loginUser };
+};
