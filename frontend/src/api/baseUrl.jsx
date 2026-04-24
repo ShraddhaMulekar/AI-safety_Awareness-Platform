926416e4198c4e771df08be870f6646e417a767a
ShraddhaMulekar <mulekarshraddha@gmail.com>
2026-04-24 22:11:02 +0530

    redeploy fix

--- frontend/src/api/baseUrl.jsx
+++ frontend/src/api/baseUrl.jsx
@@ -1,7 +1,6 @@
 // export const API_URL = "http://localhost:5000" 
 export const API_URL = "https://ai-safety-awareness-platform.onrender.com"
 
-
 export const fetchAPI = async (url, Options = {}) => {
   const token = localStorage.getItem("token")
   const headers = {
